3d7e7e5513f4b0421995126f421c0bb54beb4b03
Rajdeep <rajdeepbiswasndp@gmail.com>
2026-05-26 13:59:53 +0530

    Pydantic Field Constraints

--- Pydantic-tut/1_Pydantic_why.py
+++ Pydantic-tut/1_Pydantic_why.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel, EmailStr, AnyUrl
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
 from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     name: str
-    age: int
+    age: int = Field(gt=0, lt=200)
     weight: float
     allergies: Optional[List[str]] = None
     email: EmailStr
@@ -11,7 +11,7 @@ class Patient(BaseModel):
     
 patient_info = {
     "name": "Nitish",  
-    "age": 30,
+    "age": -30,
     "weight": 75.2,
     # "allergies": ["pollen", "dust"],
     "email": "abc@gmail.com",
