873b426a5911bb305bc555c0790877ff1207b2d5
Rajdeep <rajdeepbiswasndp@gmail.com>
2026-06-09 17:01:00 +0530

    DELETE patients data

--- main.py
+++ main.py
@@ -122,4 +122,22 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
     data[patient_id] = patient_pydantic.model_dump(exclude=["id"])
     save_data(data)
     return JSONResponse(status_code=200, content="Patient updated successfully")
+
+@app.delete("/delete/{patient_id}")
+def delete_patient(patient_id: str):
+    #load existing data
+    data = load_data()
+    
+    #check if the patient exists
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    
+    #delete the patient
+    del data[patient_id]
+    save_data(data)
+    return JSONResponse(status_code=200, content="Patient deleted successfully")
+
+    
+    
+    
     
